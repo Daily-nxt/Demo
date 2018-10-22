039b8c42991922ff3620fd35edeb21935c5426c1
admin <admin@DESKTOP-33HFTJE>
2018-10-22 12:03:59 +0800

    test java

--- TestDemo/src/com/nxt/test/deom/Test.java
+++ TestDemo/src/com/nxt/test/deom/Test.java
@@ -2,14 +2,20 @@ package com.nxt.test.deom;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
 
 	public static void main(String[] args) throws UnknownHostException {
 		// TODO Auto-generated method stub
-		InetAddress addr = InetAddress.getLocalHost();  
-        String ip=addr.getHostAddress().toString(); //获取本机ip 
-        System.out.println(ip);
+//		InetAddress addr = InetAddress.getLocalHost();  
+//        String ip=addr.getHostAddress().toString(); //获取本机ip 
+//        System.out.println(ip);
+		List<String> list = new ArrayList<String>();
+		list.add("a");
+		list.add("b");
+		System.out.println(list.toString());
 	}
 
 }
